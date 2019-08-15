09f665a890a0fcd847cc41717af986f66ab76df6
carrotFromCali <36881859+carrotFromCali@users.noreply.github.com>
2019-08-15 11:23:07 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1-An4Br7LQ7dcgjhwbWT7biW31Gokukvq'
+export_file_name = 'trained_model.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['PROD-S01-Flower', 'PROD-S01-Helmet', 'PROD-S01-IPhone', 'PROD-S01-Mouse', 'PROD-S01-T-shirt']
 path = Path(__file__).parent
 
 app = Starlette()
